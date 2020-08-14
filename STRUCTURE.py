528158fdfaed8338ce4da80a44fadaa988e8fa74
Anviksha Jain <56529970+Anvikshajain@users.noreply.github.com>
2020-08-14 09:12:35 +0530

    Update STRUCTURE.py

--- STRUCTURE.py
+++ STRUCTURE.py
@@ -1,1 +1,13 @@
+'''
+@ file name
+@ Code by author_name
+@ two line descritpion of program
+'''
 
+# return sum of two integers
+def sum(a,b):
+  return a+b
+
+print(sum(8,5))
+#comment down required line for better understanding
+#of the code
